7f0afc431b1dffc6f43e0fe7cc25f56e6f6d55fa
xloc <xloc.cc@outlook.com>
2023-05-16 23:03:27 -0700

    search ui layout, search using GPT

--- Dictionary/ContentView.swift
+++ Dictionary/ContentView.swift
@@ -6,14 +6,53 @@
 //
 
 import SwiftUI
+import ChatGPTSwift
+
+
+let API_KEY = "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"
+let api = ChatGPTAPI(apiKey: API_KEY)
 
 struct ContentView: View {
+    @State var searchedText: String = ""
+    @State var captureText: String = ""
+    @State var definition: String = ""
+    
+    
+    func onSearch () {
+        captureText = searchedText
+        searchedText = ""
+        definition = ""
+        
+        api.deleteHistoryList()
+        let prompt = "\(captureText) meaning in 3-5 sentences"
+        Task {
+            do {
+                definition = try await api.sendMessage(text: prompt)
+                // is there any other error handling ways?
+            } catch  {
+                definition = error.localizedDescription
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                TextField("Search a word here",text: $searchedText)
+                    .keyboardShortcut(.defaultAction)
+                    .onSubmit(onSearch)
+                Button(action: onSearch) {
+                    Image(systemName: "magnifyingglass.circle")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.accentColor)
+                }
+            }
+            
+            Text(captureText)
+            Text(definition)
+
+            Spacer()
         }
         .padding()
     }
